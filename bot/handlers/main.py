cf5e31f5d8d9fe846fac50de75fc2fe66cd0dc07
krolik1591 <dasenkosasa19@gmail.com>
2023-08-21 14:05:51 +0300

    is ready

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -22,29 +22,26 @@ class Check(StatesGroup):
     check = State()
 
 
-# @dp.message_handler(commands=['start', 'help'], chat_type=types.ChatType.PRIVATE)
 @router.message(F.chat.type == "private", Command('start'))
 async def start_handler(message: types.Message):
     await message.answer("Привіт. Я - бот для захисту твоїх чатів від спамерів. \n"
                          "Додай мене до чату і дай права адміністратора на кік та видалення повідомлень.")
 
 
-# @dp.message_handler(content_types=["new_chat_members"], state='*')
-@router.chat_member()
-async def on_user_join(message: types.ChatMemberUpdated, state: FSMContext):
-    print('hi')
-
+@router.chat_member(lambda member: member.new_chat_member.status == 'member')
+async def chat_member_handler(chat_member: types.ChatMemberUpdated, state: FSMContext):
     bot_id = state.bot.id
-    if message.new_chat_members[0].id == bot_id:
-        return await message.answer("Привіт, дай мені права адміністратора на кік та видалення повідомлень")
+    new_user_id = chat_member.new_chat_member.user.id
+    if new_user_id == bot_id:
+        return await state.bot.send_message(chat_member.chat.id, "Привіт, дай мені права адміністратора на кік та видалення повідомлень")
 
     question, answer = make_question()
     image_bytes = make_image(question, TIME)
 
     bot_message = await state.bot.send_photo(
-        message.chat.id,
+        chat_member.chat.id,
         photo=types.BufferedInputFile(image_bytes, filename="image.png"),
-        caption=f"@{message.new_chat_members[0].username}, відправте рішення арифметичної задачі,"
+        caption=f"@{chat_member.new_chat_member.user.username}, відправте рішення арифметичної задачі,"
                 " інакше будете додані до чорного списку чату.",
         reply_markup=types.ForceReply(selective=True)
     )
@@ -52,40 +49,29 @@ async def on_user_join(message: types.ChatMemberUpdated, state: FSMContext):
     await state.update_data(
         answer=answer,
         bot_message=bot_message,
-        new_member_message=message
     )
     await state.set_state(Check.check)
     await asyncio.sleep(TIME)
+
     data = await state.get_data()
     if data and data['bot_message'] == bot_message:  # Если пользователь не ответил то data еще не очищен
-        await kick_user(message, state)  # и bot_message-ы совпадают.   в таком случае кикаем
+        await kick_user(state, chat_member.chat.id, new_user_id, chat_member.new_chat_member.user.username)  # и bot_message-ы совпадают.   в таком случае кикаем
         await state.set_state(None)  # и очищаем data и state
-    # with suppress(exceptions.MessageToDeleteNotFound):
-    #     await bot_message.delete()
-    #     await message.delete()
 
 
-# @dp.message_handler(state=Check.check, content_types=types.ContentType.ANY)
-# @router.message()
 @router.message(StateFilter(Check.check))
 async def answer_handler(message: types.Message, state: FSMContext):
     data = await state.get_data()
     is_check_passed = message.text and str(data['answer']) in message.text
 
     if not is_check_passed:
-        await kick_user(message, state)
+        await kick_user(state, message.chat.id, message.from_user.id, message.from_user.username)
 
-    await data['bot_message'].delete()
-
-    # try:
-    #     await message.delete()
-    #     if not is_check_passed:
-    #         await data['new_member_message'].delete()
-    # except exceptions.MessageCantBeDeleted:
-    #     if message.chat.type != types.ChatType.SUPERGROUP:
-    #         await message.answer('У мене немає прав на видалення повідомлень, '
-    #                              'тому я буду спамити цим попередженням :)')
-    # await state.finish()  # Пользователь ответил, стейт = None
+    bot_message = data['bot_message']
+    await state.bot.delete_message(bot_message.chat.id, bot_message.message_id)
+    await message.delete()
+    await state.update_data(bot_message=None)
+    await state.set_state(None)
 
 
 def make_question():
@@ -95,8 +81,9 @@ def make_question():
     return question, answer
 
 
-async def kick_user(message: types.Message, state):
-    await state.bot.ban_chat_member(message.chat.id, message.from_user.id, untAil_date=int(time()) + 35) # бан на 35 сек. <30 = inf
-    await state.bot.unban_chat_member(message.chat.id, message.from_user.id)
-    # except exceptions.ChatAdminRequired:
-    #     await bot.send_message(chat.id, f'Я б із задоволенням кікнув {user.mention}, але в мене немає адмінки :(')
+async def kick_user(state, chat_id, user_id, username):
+    try:
+        await state.bot.ban_chat_member(chat_id, user_id, until_date=int(time()) + 35) # бан на 35 сек. <30 = inf
+        await state.bot.unban_chat_member(chat_id, user_id)
+    except exceptions.TelegramBadRequest:
+        await state.bot.send_message(chat_id, f'Не можу кікнути юзера або видалити повідомлення, дайте адмінку')
